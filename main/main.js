7b5e8bda9ad39baf8e1e4a8e2fe2254343055b74
masidaxiaoming <1040412498@qq.com>
2017-05-24 15:39:24 +0800

    余数计算main函数

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(a,b) { 
+	var c;
+	if (b == 0){
+		console.log("除数不能为0！");
+		return 'error:除数不能为0！';
+	}else{
+		c = a%b; 
+		console.log("余数为：" + c);
+		return c;
+	}	
 };
